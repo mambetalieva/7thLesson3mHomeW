09db72d50d83c0f4f4909912c494dbe538b90e01
kayra <mgulkajyr@inbox.ru>
2023-03-10 01:44:44 +0600

    Initial Commit

---
+++ 7thLesson3mHomeW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7thLesson3mHomeW
+//
+//  Created by Каира on 10.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
